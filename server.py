7d397452401420c77cf048518e2818654ca132bb
David Ng <davidnng1999@gmail.com>
2025-07-22 18:37:30 +0530

    Create server.py

---
+++ server.py
@@ -0,0 +1,16 @@
+from flask import Flask, send_from_directory
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return send_from_directory('.', 'index.html')
+
+@app.route('/static/<path:filename>')
+def static_files(filename):
+    return send_from_directory('static', filename)
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=os.getenv('PORT', 5000))
+  
